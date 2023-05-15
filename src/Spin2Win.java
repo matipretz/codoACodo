8d0bb4242283a2d33bfcb97dc628f8a88172bf7e
matipretz <matipretz@gmail.com>
2023-05-15 20:39:21 -0300

    modified:   src/Spin2Win.java

--- src/Spin2Win.java
+++ src/Spin2Win.java
@@ -1,10 +1,6 @@
 public class Spin2Win {
   public static int fichas = 1000;// Almacena de forma constante la cantidad de fichas del jugador.
 
-  public static void main(String[] args) {
-    juego();
-  }
-
   public static void juego() {
     S2WFunction fun = new S2WFunction();
     while (true) {

3ff252b728c888e72c89abdffada6cd8f24386c6
matipretz <matipretz@gmail.com>
2023-05-31 01:01:02 -0300

    new file:   src/Funciones.java 	modified:   src/desafio4Arreglos.java

---
+++ src/Funciones.java
@@ -0,0 +1,4 @@
+//Crear un Hola mundo de manera tradicional
+//2. Crear un método static sin recepcion de valores
+//3. Crear un método static con recepcion de valores
+//4. Crear un método static para despedir
--- src/desafio4Arreglos.java
+++ src/desafio4Arreglos.java
@@ -9,27 +9,31 @@ public class Desafio4Arreglos {
         System.out.println("#                                                                             #");
         System.out.println("###############################################################################\n");
         Scanner sc = new Scanner(System.in);
+        int[] arreglo = new int[10];
 
-        /*
+        /*###############################################################################
         Desafío de clase - 1
         ● Hacer un array que me permita la carga por teclado de
         10 notas de clases, terminada la carga arrojará, las notas
         cargadas y el promedio de ellas.
-        */
+       ############################################################################### */
         System.out.println("Bienvenido. Ingrese notas:");
-        int[] arreglo = new int[10];
+        double sum = 0;
         for (int i = 0; i < arreglo.length; i++) {
             arreglo[i] = sc.nextInt();
+            sum+=arreglo[i];
         }
-        sc.close();
     
-      System.out.println(Arrays.toString(arreglo));
+      System.out.println("Arreglo: " + Arrays.toString(arreglo));
+      System.out.println("Suma: " + sum);
+      System.out.println("Primedio: " + sum/arreglo.length);
 
 
 
 
 
-        /*
+
+        /*###############################################################################
         Desafío de clase - 2
         ● Programar una aplicación que utilice un arraylist
         para cargar un listado de cosas a comprar en el
@@ -37,7 +41,9 @@ public class Desafio4Arreglos {
         ● Permitir al usuario cargar en un buscador el
         producto y si el producto está en el listado que
         arroje un aviso “Si, tenés que comprar el producto”
+        ###############################################################################*/
 
+        /*###############################################################################
         Desafío de clase 3
         ● Hacer un array que me permita la carga por teclado el
         tamaño del arreglo, luego cargar datos, terminada la
@@ -46,9 +52,9 @@ public class Desafio4Arreglos {
         https://app.codingrooms.com/w/8sZ6zg6rD4FY
         */
 
-      /*   System.out.println("Presiona [Enter] para volver al menu principal.");
+        System.out.println("Presiona [Enter] para volver al menu principal.");
         sc.nextLine();
         sc.close();
-        Menu.menu();*/
+        Menu.menu();
     }
 }
